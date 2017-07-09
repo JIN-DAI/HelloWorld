cfc4bfc859d3b7d93191714e0b848c66f59f5944
Jin Dai <daijinghub@163.com>
2017-07-09 18:46:07 +0200

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding=utf-8
+#author='J.Dai'
+
+print("Hello world!")
+
+def test():
+    now_time = time.time()
+    a = 10
+    a += 1
+    b = 'test string'
+
+if __name__ == '__main__':
+    test()
---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding=utf-8
+"""
+@version: 1.0
+@author: jin.dai
+@license: Apache Licence
+@contact: daijing491@gmail.com
+@software: PyCharm
+@file: test.py
+@time: 2017/7/9 16:52
+"""
+
+if __name__ == '__main__':
+    pass
