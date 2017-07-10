ade234d7e41128603e3b2790a2ab1d79be75f6e8
JIN-DAI <daijing491@gmail.com>
2017-07-10 12:01:07 +0200

    Changes on README.md and hello_world.py from local

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-##HelloWorld
+##HelloWorld##
 
-###项目描述
+###项目描述###
 
 *This is just a test project of sharing repository from Pycharm to Github.*
--- hello_world.py
+++ hello_world.py
@@ -2,9 +2,11 @@
 #coding=utf-8
 #author='J.Dai'
 
+import time
 
 def test():
     now_time = time.time()
+    print("Now time:%d"%now_time)
     a = 10
     a += 1
     b = 'test string'
@@ -13,3 +15,4 @@ if __name__ == '__main__':
     test()
     print("Hello world!")
     print("Hello Git!")
+    print("Bye-Bye")
